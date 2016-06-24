6d4487777fd9c367739e742029aa8d2d6401e614
Abdullah Bakhsh <empiricalthemes@gmail.com>
2016-06-24 12:32:07 -0400

    Use 'react-native-mock' instead of manually mocking RN

--- __mocks__/react-native.js
+++ __mocks__/react-native.js
@@ -1,20 +1,3 @@
 'use strict';
 
-var React = require('react');
-var ReactNative = React;
-
-ReactNative.StyleSheet = {
-  create: function(styles) {
-    return styles;
-  }
-};
-
-class View extends React.Component {}
-class Text extends React.Component {}
-class TouchableHighlight  extends React.Component {}
-
-ReactNative.View = View;
-ReactNative.Text = Text;
-ReactNative.TouchableHighlight = TouchableHighlight;
-
-module.exports = ReactNative;
+module.exports = require('react-native-mock');
